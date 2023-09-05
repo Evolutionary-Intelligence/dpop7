ffb7dfe979741e99bd7e86fcbdee65d41f4453cf
Evolutionary-Intelligence <78018333+Evolutionary-Intelligence@users.noreply.github.com>
2023-09-05 16:13:35 +0800

    Add files via upload

---
+++ dpop7/optimizers/dpso/_test_dspso.py
@@ -0,0 +1,27 @@
+import ray  # engine for distributed computing
+import numpy  # engine for numerical computing
+from pypop7.benchmarks.base_functions import rosenbrock  # function to be minimized
+from dpop7.optimizers.dpso.dspso import DSPSO  # Distributed Standard Particle Swarm Optimizer
+
+
+if __name__ == '__main__':
+    ray.init()  # to configure the clustering computing platform here
+
+
+    @ray.remote
+    def ff(x):  # fitness_function
+        return rosenbrock(x)
+
+
+    problem = {'fitness_function': ff,  # define problem arguments
+               'ndim_problem': 2000,
+               'lower_boundary': -5.0*numpy.ones((2000,)),
+               'upper_boundary': 5.0*numpy.ones((2000,))}
+    options = {'max_function_evaluations': 50000*2,  # set optimizer options
+               'n_individuals': 10,  # number of parallel samples (individuals)
+               'seed_rng': 2023}
+    dspso = DSPSO(problem, options)  # initialize the optimizer class
+    results = dspso.optimize()  # run the optimization process
+    # return the number of used function evaluations and found best-so-far fitness
+    print(f"DSPSO: {results['n_function_evaluations']}, {results['best_so_far_y']}")
+    # DSPSO: 100000, 25736726.758652825
