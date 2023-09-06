c59bfae4caf15bec407266ec8d8d8b3bfa770b9e
Evolutionary-Intelligence <78018333+Evolutionary-Intelligence@users.noreply.github.com>
2023-09-06 22:02:00 +0800

    Update dspso.py

--- dpop7/optimizers/dpso/dspso.py
+++ dpop7/optimizers/dpso/dspso.py
@@ -45,8 +45,8 @@ class DSPSO(DPSO):
        ...     return rosenbrock(x)
        >>> problem = {'fitness_function': f,  # define problem arguments
        ...            'ndim_problem': 2,
-       ...            'lower_boundary': -5*numpy.ones((2,)),
-       ...            'upper_boundary': 5*numpy.ones((2,))}
+       ...            'lower_boundary': -5.0*numpy.ones((2,)),
+       ...            'upper_boundary': 5.0*numpy.ones((2,))}
        >>> options = {'max_function_evaluations': 5000,  # set optimizer options
        ...            'seed_rng': 2022}  # seed for random number generation
        >>> dspso = DSPSO(problem, options)  # initialize the optimizer class
